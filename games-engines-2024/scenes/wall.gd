47105061277fc8850af0e6d913b68590f0639c1b
Eric O'Brien <ericobrien246@gmail.com>
2024-10-06 22:57:30 +0100

    update project

--- games-engines-2024/scenes/wall.gd
+++ games-engines-2024/scenes/wall.gd
@@ -7,29 +7,20 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-<<<<<<< HEAD
 	spawn_tower()
 
 	pass # Replace with function body.
 
 func spawn_tower():
-=======
->>>>>>> parent of dbb4df2 (add colour to bricks)
+
 	for row in range(rows):
 		for col in range(cols):
 			var brick = brick_scene.instantiate()
 			var pos = Vector3(col, row, 0)
 			brick.position = pos
 			add_child(brick)
-<<<<<<< HEAD
-			
-=======
-
 	pass # Replace with function body.
 
-
-
->>>>>>> parent of dbb4df2 (add colour to bricks)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
