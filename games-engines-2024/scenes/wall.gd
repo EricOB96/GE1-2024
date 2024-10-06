ade8a3754f5fae6f1cc6f94ec3b4961e4992ae9c
Eric O'Brien <ericobrien246@gmail.com>
2024-10-06 22:23:52 +0100

    update project

--- games-engines-2024/scenes/wall.gd
+++ games-engines-2024/scenes/wall.gd
@@ -3,30 +3,33 @@ extends Node3D
 @export var brick_scene:PackedScene
 @export var rows = 10
 @export var cols = 10
-@export var bricks_no: int = 10
-@export var radius: float = 1.0
-@export var height : float = 1.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+<<<<<<< HEAD
 	spawn_tower()
 
 	pass # Replace with function body.
 
 func spawn_tower():
+=======
+>>>>>>> parent of dbb4df2 (add colour to bricks)
 	for row in range(rows):
 		for col in range(cols):
 			var brick = brick_scene.instantiate()
 			var pos = Vector3(col, row, 0)
 			brick.position = pos
-			
-			var m = StandardMaterial3D.new()
-			var h = ((row * cols) + col) / (float)(rows * cols)
-			m.albedo_color = Color.from_hsv(h, 1, 1)
-			var mesh:MeshInstance3D = brick.get_node("MeshInstance3D")
-			mesh.set_surface_override_material(0, m)
 			add_child(brick)
+<<<<<<< HEAD
 			
+=======
+
+	pass # Replace with function body.
+
+
+
+>>>>>>> parent of dbb4df2 (add colour to bricks)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
